4fec8a4685d8341c66fb66c6e4d6115c3fff5e6a
ShauryaBansal15 <bansa176@purdue.edu>
2026-01-07 18:30:19 -0600

    Made some changes by adding a visual for basic Student fields

--- GradebookGUI.java
+++ GradebookGUI.java
@@ -1,8 +1,19 @@
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+import javax.swing.JButton;
+import javax.swing.SwingUtilities;
+import java.awt.BorderLayout;
+
 
 public class GradebookGUI extends JFrame {
     private GradebookManager manager;
     private JTable studentTable;
+    private DefaultTableModel tableModel;
+    private JScrollPane scrollPane;
     private JTextField firstNameField, lastNameField, idField, scoresField;
     private JButton addStudentButton, removeStudentButton, sortByNameButton, sortByIDButton;
     
@@ -10,6 +21,10 @@ public class GradebookGUI extends JFrame {
         manager = new GradebookManager();
 
         setupFrame();
+        setupTable();
+        setupButtons();
+        setupLayout();
+
 
         setVisible(true);
     }
@@ -21,11 +36,34 @@ public class GradebookGUI extends JFrame {
         setLocationRelativeTo(null); // Centers on the screen
     }
 
-    private void setupComponents() {
+    private void setupTable() {
+        String[] columnNames = {"Student ID", "First Name", "Last Name", "Average"};
+
+
+        tableModel = new DefaultTableModel(columnNames, 0) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false; // Makes cells non-editable
+            }
+        };
+
+        studentTable = new JTable(tableModel);
+
+        // Enables single row selection
+        studentTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+        // Wrap table in a scroll pane (make it scrollable)
+        scrollPane = new JScrollPane(studentTable);
+
+    }
+
+    private void setupButtons() {
 
     }
 
     private void setupLayout() {
+        //  Adding the scroll pane (which contains the table) to the center
+        add(scrollPane, BorderLayout.CENTER);
 
     }
 
@@ -38,7 +76,19 @@ public class GradebookGUI extends JFrame {
     }
 
     private void refreshTable() {
-        
+        // Clears existing rows
+        tableModel.setRowCount(0);
+
+        // Add each student as a row
+        for (Student s : manager.getAllStudents()) {
+            Object[] row = {
+                s.getStudentID(),
+                s.getFirstName(),
+                s.getLastName(),
+                String.format("%.2f", s.getAverage())
+            };
+            tableModel.addRow(row);
+        }
     }
 
     public static void main(String[] args) {
